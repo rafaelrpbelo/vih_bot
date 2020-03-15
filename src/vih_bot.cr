9355c3b8ae2a8a54a5667d177ce10270943a998a
Rafael Belo <rafaelrpbelo@gmail.com>
2020-03-14 22:51:28 -0300

    Create a minimal bot app

--- src/vih_bot.cr
+++ src/vih_bot.cr
@@ -1,6 +1,65 @@
-# TODO: Write documentation for `VihBot`
+require "logger"
+require "kemal"
+
 module VihBot
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  TELEGRAM_API_HOST = "https://api.telegram.org/"
+  TELEGRAM_BOT_TOKEN = ENV.fetch("TELEGRAM_BOT_TOKEN") do
+    raise ArgumentError.new("TELEGRAM_BOT_TOKEN env var must be assigned.")
+  end
+
+  class Server
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
+    def set_filters
+      before_all "/:token" do |env|
+        unless authorized?(env.params.url["token"])
+          halt env, status_code: 401, response: "Unauthorized"
+        end
+      end
+    end
+
+    def set_routes
+      post "/:token" do |env|
+        begin
+          message = env.params.json["message"]
+
+          body = message.is_a?(Hash(String, JSON::Any)) ? message["text"] : ""
+          from = message.is_a?(Hash(String, JSON::Any)) ? message["from"] : { String => String }
+
+          username = from["username"]
+        rescue e : Exception
+          logger.error("Wrong parameters!")
+          halt env, status_code: 500, response: "Wrong parameters"
+        end
+
+        logger.info("I got a message from #{username}. Message: #{body}.")
+
+        "ok"
+      end
+    end
+
+    def authorized?(token : String)
+      token === TELEGRAM_BOT_TOKEN
+    end
+
+    def run
+      set_filters
+      set_routes
+      Kemal.run
+    end
+
+    def self.run
+      new.run
+    end
+  end
+
+  def self.run
+    Server.run
+  end
 end
+
+VihBot.run
