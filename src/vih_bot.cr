883abbb5a821aada070d68707cd78424273a9850
Rafael Belo <rafaelrpbelo@gmail.com>
2020-03-15 23:54:39 -0300

    Enable bot to say hello

--- src/vih_bot.cr
+++ src/vih_bot.cr
@@ -1,5 +1,6 @@
 require "logger"
-require "kemal"
+
+require "./vih_bot/server"
 
 module VihBot
   VERSION = "0.1.0"
@@ -9,54 +10,9 @@ module VihBot
     raise ArgumentError.new("TELEGRAM_BOT_TOKEN env var must be assigned.")
   end
 
-  class Server
-    def logger
-      @logger ||= Logger.new(STDOUT)
-    end
-
-    def set_filters
-      before_all "/:token" do |env|
-        unless authorized?(env.params.url["token"])
-          halt env, status_code: 401, response: "Unauthorized"
-        end
-      end
-    end
-
-    def set_routes
-      post "/:token" do |env|
-        logger.info("Params got: #{env.params.json.to_s}")
-
-        message = env.params.json["message"]
-
-        body = message.is_a?(Hash(String, JSON::Any)) ? message["text"] : ""
-        from = message.is_a?(Hash(String, JSON::Any)) ? message["from"] : { String => String }
-
-        username = from["username"]
-
-        logger.info("I got a message from #{username}. Message: #{body}.")
-
-        "ok"
-      end
-    end
-
-    def authorized?(token : String)
-      token === TELEGRAM_BOT_TOKEN
-    end
-
-    def run
-      set_filters
-      set_routes
-      Kemal.run
-    end
-
-    def self.run
-      new.run
-    end
-  end
-
-  def self.run
-    Server.run
+  def self.logger
+    @@logger ||= Logger.new(STDOUT)
   end
 end
 
-VihBot.run
+VihBot::Server.run
---
+++ src/vih_bot/server.cr
@@ -0,0 +1,59 @@
+require "kemal"
+require "json"
+
+module VihBot
+  class Server
+    def logger
+      VihBot.logger
+    end
+
+    def setup
+      before_all "/:token" do |env|
+        unless authorized?(env.params.url["token"])
+          halt env, status_code: 401, response: "Unauthorized"
+        end
+      end
+
+      post "/:token" do |env|
+        logger.info("Params: #{env.params.json.to_s}")
+
+        payload = env.params.json
+
+        message   = payload["message"]?
+        text      = message.is_a?(Hash(String, JSON::Any)) ? message["text"] : nil
+        from      = message.is_a?(Hash(String, JSON::Any)) ? message["from"] : nil
+        username  = from.is_a?(JSON::Any) ? from["username"] : nil
+        chat      = message.is_a?(Hash(String, JSON::Any)) ? message["chat"] : nil
+        chat_id   = chat.is_a?(JSON::Any) ? chat["id"] : nil
+
+        if text && username
+          logger.info("I got a message from #{username}. Message: #{text}.")
+        end
+
+        if chat_id && text == "Hello Vih"
+          {
+            sendMessage: {
+              chat_id: chat_id,
+              text: "Hi there! ;)"
+            }
+          }.to_json
+        else
+          "OK"
+        end
+      end
+    end
+
+    def authorized?(token : String)
+      token === TELEGRAM_BOT_TOKEN
+    end
+
+    def run
+      setup
+      Kemal.run
+    end
+
+    def self.run
+      new.run
+    end
+  end
+end
